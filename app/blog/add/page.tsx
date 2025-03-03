8c0769a701a28639faf45de81b2e01443756a870
asfarmunir <asfarmunir12345@gmail.com>
2025-03-03 17:47:11 +0500

    ui update

--- app/blog/add/page.tsx
+++ app/blog/add/page.tsx
@@ -1,4 +1,12 @@
 "use client";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Image from "next/image";
 import React, { useState, ChangeEvent, FormEvent } from "react";
 
 interface FormData {
@@ -126,8 +134,8 @@ const page: React.FC = () => {
   };
 
   return (
-    <div className="px-5 md:px-12">
-      <div className="py-6 xl:py-[32px] border-b border-[#EBE9E0] pb-5 mb-9">
+    <div className="">
+      <div className="py-6  px-5 md:px-12 xl:px-[80px] xl:py-[32px] border-b border-[#EBE9E0] pb-5 mb-9">
         <h1 className="text-xs text-[#919089]">
           Home / Blog / Blog Post Title{" "}
         </h1>
@@ -135,26 +143,36 @@ const page: React.FC = () => {
           write a blog post
         </h1>
       </div>
-      <div className=" w-full py-8 flex items-start gap-12">
-        <div className="max-w-xl mx-auto p-6 md:p-8 flex-grow bg-primary-50 shadow ">
+      <div className=" w-full px-5 md:px-12 xl:px-[80px] py-8 h-full flex items-start gap-12">
+        <div className=" mx-auto p-6 md:p-8 flex-grow bg-primary-50  ">
           <form onSubmit={handleSubmit} className="space-y-5 ">
             {/* Category Field */}
             <div>
-              <label className="block text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
+              <label className="block tracking-wide text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
                 Category
               </label>
-              <select
+              {/* <select
                 name="category"
                 value={formData.category}
                 onChange={handleInputChange}
-                className="w-full border-[#EBE9E0]   shadow-sm p-2 text-xs 2xl:text-sm "
+                className="w-full border-[#EBE9E0]    p-2 text-xs 2xl:text-sm "
               >
                 <option value="">-Select-</option>
                 <option value="Technology">Technology</option>
                 <option value="Health">Health</option>
                 <option value="Lifestyle">Lifestyle</option>
                 <option value="Business">Business</option>
-              </select>
+              </select> */}
+              <Select>
+                <SelectTrigger className="w-full text-xs 2xl:text-sm mt-1 xl:p-4 rounded-none  border border-[#EBE9E0] text-[#919089] ">
+                  <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
               {errors.category && (
                 <p className="text-red-500 text-sm mt-1">{errors.category}</p>
               )}
@@ -162,7 +180,7 @@ const page: React.FC = () => {
 
             {/* Title Field */}
             <div>
-              <label className="block text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
+              <label className="block tracking-wide text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
                 Title
               </label>
               <input
@@ -171,7 +189,7 @@ const page: React.FC = () => {
                 value={formData.title}
                 onChange={handleInputChange}
                 placeholder="Title for your Blog post"
-                className="w-full border-[#EBE9E0]  shadow-sm p-2 text-xs 2xl:text-sm "
+                className="w-full border-[#EBE9E0]   p-2  xl:p-4 text-xs 2xl:text-sm "
               />
               {errors.title && (
                 <p className="text-red-500 text-sm mt-1">{errors.title}</p>
@@ -180,7 +198,7 @@ const page: React.FC = () => {
 
             {/* Subtitle Field */}
             <div>
-              <label className="block text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
+              <label className="block tracking-wide text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
                 Subtitle
               </label>
               <textarea
@@ -189,26 +207,27 @@ const page: React.FC = () => {
                 onChange={handleInputChange}
                 placeholder="Subtitle"
                 maxLength={300}
-                className="w-full border-[#EBE9E0]  shadow-sm p-2 text-xs 2xl:text-sm  h-28"
+                className="w-full border-[#EBE9E0]   p-2  xl:p-4 text-xs 2xl:text-sm  h-28"
               />
-              <p className="text-gray-500 text-sm text-right">
+              <p className="text-[#919089] text-xs text-left">
                 {formData.subtitle.length} / 300
               </p>
             </div>
 
             {/* Header Image Upload */}
             <div>
-              <label className="block text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
+              <label className="block tracking-wide text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
                 Upload a Header Image
               </label>
               {/* <input
               type="file"
               accept="image/*"
               onChange={handleImageUpload}
-              className="w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100"
+              className="w-full text-sm text-[#919089] file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100"
             /> */}
-              <div className=" w-full bg-white flex items-center justify-center h-28">
-                <p className="text-brown-200 text-xs uppercase">
+              <div className=" w-full bg-white border border-dashed flex items-center justify-center h-28">
+                <p className="text-[#919089] text-center text-xs uppercase">
+                  Drag and Drop <br /> or <br />
                   Upload image here
                 </p>
               </div>
@@ -225,7 +244,7 @@ const page: React.FC = () => {
                   Section {index + 1}
                 </h3>
                 <div>
-                  <label className="block text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
+                  <label className="block tracking-wide text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
                     Header
                   </label>
                   <input
@@ -238,7 +257,7 @@ const page: React.FC = () => {
                   />
                 </div>
                 <div>
-                  <label className="block text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
+                  <label className="block tracking-wide text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
                     Paragraph
                   </label>
                   <textarea
@@ -250,7 +269,7 @@ const page: React.FC = () => {
                   />
                 </div>
                 <div>
-                  <label className="block text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
+                  <label className="block tracking-wide text-xs 2xl:text-sm font-medium text-[#0D0106] uppercase mb-2">
                     Image
                   </label>
                   {/* <input
@@ -259,8 +278,9 @@ const page: React.FC = () => {
                   onChange={(e) => handleImageUpload(e, index)}
                   className="w-full text-sm text-gray-500"
                 /> */}
-                  <div className=" w-full bg-white flex items-center justify-center h-28">
-                    <p className="text-brown-200 text-xs uppercase">
+                  <div className=" w-full bg-white border border-dashed flex items-center justify-center h-28">
+                    <p className="text-[#919089] text-center text-xs uppercase">
+                      Drag and Drop <br /> or <br />
                       Upload image here
                     </p>
                   </div>
@@ -282,21 +302,42 @@ const page: React.FC = () => {
             {/* Submit Button */}
             <button
               type="submit"
-              className="w-full bg-[#0D0106] text-white py-3
-             px-4 text-xs 2xl:text-sm uppercase font-thin  shadow-sm  focus:ring-2  focus:ring-offset-2"
+              className="w-full bg-[#0D0106] text-white py-4
+             px-4 text-xs 2xl:text-sm uppercase font-thin    focus:ring-2  focus:ring-offset-2"
             >
               PUBLISH blog post
             </button>
           </form>
         </div>
-        <div className=" space-y-5  flex-grow">
+        <div className=" space-y-5 border-l border-dashed px-8 h-full flex-grow">
           <h2 className=" font-playfair  uppercase">Preview</h2>
-          <div className=" w-full  bg-primary-50 p-28"></div>
+          <div className=" w-full  bg-[#A6A6A6] p-28 py-48"></div>
           <h2 className="text-2xl font-playfair">Blog Title</h2>
-          <span className="text-xs text-[#666666] ">18 Jan 2022</span>
-          <p className=" max-w-lg text-sm text-[#666666]">
+          <div className="flex flex-row items-center text-sm gap-3 pb-3">
+            <div className="bg-[#A6A6A6] p-2 rounded-full w-8 h-8 flex items-center justify-center">
+              <Image
+                src="/"
+                width={10}
+                height={10}
+                className="rounded-full"
+                alt=""
+              />
+            </div>
+            <h1 className="text-[14px] font-normal uppercase text-[#463F3A]">
+              Oliver bennett
+            </h1>
+            <span className="w-1 h-1 bg-[#463F3A] rounded-full"></span>
+            <h1 className="text-[#463F3A] uppercase text-[14px]">
+              23 JAN 2023
+            </h1>
+          </div>{" "}
+          <p className=" max-w-lg pb-80 text-sm text-[#666666]">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
-            quaerat neque nemo iure eos officiis sit
+            quaerat neque nemo iure eos officiis sit <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
+            quaerat neque nemo iure eos officiis sit <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
+            quaerat neque nemo iure eos officiis sit <br />
           </p>
         </div>
       </div>
